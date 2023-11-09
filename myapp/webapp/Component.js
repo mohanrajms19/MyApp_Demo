af9dddcb68e82f9fff8013be01ea6156c7fbce3b
unknown <rajeev@inflexiontechfze.com>
2023-11-09 09:16:51 +0530

    All

--- myapp/webapp/Component.js
+++ myapp/webapp/Component.js
@@ -5,9 +5,10 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "com/myapp/model/models"
+        "com/myapp/model/models",
+        "sap/ui/model/json/JSONModel"
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models,JSONModel) {
         "use strict";
 
         return UIComponent.extend("com.myapp.Component", {
